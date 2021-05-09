158307f08587790141b73252addae39254551830
Will Mann <will@willmann.me.uk>
2021-05-10 00:30:54 +0100

    App Icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,25 +27,30 @@ function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <link rel='shortcut icon' href='/images/favicon.ico' />
         <link
           rel='apple-touch-icon'
           sizes='180x180'
-          href='/apple-touch-icon.png'
+          href='/images/apple-touch-icon.png'
         />
         <link
           rel='icon'
           type='image/png'
           sizes='32x32'
-          href='/favicon-32x32.png'
+          href='/images/favicon-32x32.png'
         />
         <link
           rel='icon'
           type='image/png'
           sizes='16x16'
-          href='/favicon-16x16.png'
+          href='/images/favicon-16x16.png'
         />
-        <link rel='manifest' href='/manifest.json' />
-        <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#144733' />
+        <link rel='manifest' href='/images/manifest.json' />
+        <link
+          rel='mask-icon'
+          href='/images/safari-pinned-tab.svg'
+          color='#144733'
+        />
         <meta name='apple-mobile-web-app-title' content='Track &amp; Trace' />
         <meta name='application-name' content='Track &amp; Trace' />
         <meta name='msapplication-TileColor' content='#144733' />
